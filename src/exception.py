9c8962888d34dfcfc8361cd7d5aa68aa9363365a
Aman Gupta <Amank8525@gmail.com>
2025-02-10 21:10:30 +0530

    Flask Code

--- src/exception.py
+++ src/exception.py
@@ -1,15 +1,15 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb =error_detail.exc_info()
+def error_message_detail(error, error_detail):
+    _,_,exc_tb =sys.exc_info()
     filename = exc_tb.tb_frame.f_code.co_filename
     error_message = f"Error occured in python script name: {filename} at line number: {exc_tb.tb_lineno} with error message: {str(error)}"
 
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail= error_detail)
 
